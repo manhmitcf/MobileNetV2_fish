c3ab8b520473bb111979ad7e482d5ae529f1af00
manhmitcf <manhmit1221@gmail.com>
2025-04-02 16:17:29 +0700

    ok

--- src/model.py
+++ src/model.py
@@ -1,12 +1,13 @@
 import torch
 import torch.nn as nn
 import torchvision.models as models
+from torchvision.models import MobileNet_V2_Weights
 
 class FishClassifier(nn.Module):
     def __init__(self, num_classes=8):
         super(FishClassifier, self).__init__()
         # Sử dụng MobileNetV2 thay vì ResNet
-        self.mobilenetv2 = models.mobilenet_v2(pretrained=True)
+        self.mobilenetv2 = models.mobilenet_v2(weights = MobileNet_V2_Weights.IMAGENET1K_V1)
         
         # Thay thế Fully Connected Layer cuối cùng để có đúng số lượng lớp
         self.mobilenetv2.classifier[1] = nn.Sequential(
