4715bea86b399c2a255f05a294ccf4efa8e25e75
manhmitcf <manhmit1221@gmail.com>
2025-04-02 22:16:14 +0700

    ok

--- src/aug.py
+++ src/aug.py
@@ -20,7 +20,6 @@ augment_transform = transforms.Compose([
     transforms.RandomHorizontalFlip(),
     transforms.RandomRotation(15),
     transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
-    transforms.RandomResizedCrop(224, scale=(0.8, 1.0)),
     transforms.ToTensor()
 ])
 
